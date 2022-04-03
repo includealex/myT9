93f408419b00826f136c12f10dfee19bb1eb78f0
includealex <mishin.aa@phystech.edu>
2022-04-03 20:28:31 +0300

    bench for val to realloc

---
+++ hashtable/bench/reallocsize.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+
+#include "hash_table.hpp"
+#include "hash_table_impl.hpp"
+
+static void htableBench(benchmark::State& addit) {
+    auto len = static_cast<double>(addit.range(0));
+
+    for(auto _ : addit) {
+        HashTable<int> first;
+        double startval = 1;
+        change_realloc_size(first, static_cast<double>(len / 10), startval);
+
+        for(size_t i = 0; i < 1e6; ++i) {
+            first.addelem(i);
+        }
+    }
+}
+
+BENCHMARK(htableBench)->DenseRange(1, 30)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
