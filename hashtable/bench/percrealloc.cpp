1006cbb85fe7fd56be56df0a0f98b108c7dee0b0
includealex <mishin.aa@phystech.edu>
2022-04-08 02:47:58 +0300

    formatted

--- hashtable/bench/percrealloc.cpp
+++ hashtable/bench/percrealloc.cpp
@@ -4,17 +4,17 @@
 #include "hash_table_impl.hpp"
 
 static void htableBench(benchmark::State& addit) {
-    auto len = static_cast<double>(addit.range(0));
+  auto len = static_cast<double>(addit.range(0));
 
-    for(auto _ : addit) {
-        HashTable<int> first;
-        double startval = 0;
-        change_realloc_perc(first, static_cast<double>(len / 10), startval);
+  for (auto _ : addit) {
+    HashTable<int> first;
+    double startval = 0;
+    change_realloc_perc(first, static_cast<double>(len / 10), startval);
 
-        for(size_t i = 0; i < 1e6; ++i) {
-            first.addelem(i);
-        }
+    for (size_t i = 0; i < 1e6; ++i) {
+      first.addelem(i);
     }
+  }
 }
 
 BENCHMARK(htableBench)->DenseRange(1, 9)->Unit(benchmark::kMillisecond);
--- hashtable/bench/reallocsize.cpp
+++ hashtable/bench/reallocsize.cpp
@@ -4,17 +4,17 @@
 #include "hash_table_impl.hpp"
 
 static void htableBench(benchmark::State& addit) {
-    auto len = static_cast<double>(addit.range(0));
+  auto len = static_cast<double>(addit.range(0));
 
-    for(auto _ : addit) {
-        HashTable<int> first;
-        double startval = 1;
-        change_realloc_size(first, static_cast<double>(len / 10), startval);
+  for (auto _ : addit) {
+    HashTable<int> first;
+    double startval = 1;
+    change_realloc_size(first, static_cast<double>(len / 10), startval);
 
-        for(size_t i = 0; i < 1e6; ++i) {
-            first.addelem(i);
-        }
+    for (size_t i = 0; i < 1e6; ++i) {
+      first.addelem(i);
     }
+  }
 }
 
 BENCHMARK(htableBench)->DenseRange(1, 30)->Unit(benchmark::kMillisecond);
