a27d72ab9e21a99c86ce1fb90f15763afeb9fb32
includealex <mishin.aa@phystech.edu>
2022-04-12 06:36:47 +0300

    editor functional implemented

---
+++ editor/includes/editor.hpp
@@ -0,0 +1,38 @@
+#ifndef EDITOR_INCLUDES_EDITOR_HPP
+#define EDITOR_INCLUDES_EDITOR_HPP
+
+#include "dictionary.hpp"
+
+#include <vector>
+#include <set>
+
+class Editor {
+ public:
+  Editor();
+  Editor(const Editor& other);
+  Editor(Editor&& other) noexcept;
+  ~Editor();
+  
+  Editor& operator=(const Editor& other);
+  Editor& operator=(Editor&& other) noexcept;
+  bool operator==(const Editor& other) const;
+  bool operator!=(const Editor& other) const;
+
+  void add_word(const std::string& str);
+  bool is_dict(const size_t& wsize) const;
+  size_t total_words() const;
+
+  std::string find_fit_word(std::string& str) const;
+
+  std::vector<std::string> get_dict_words(const size_t& wsize) const;
+  void print_dict_words(const size_t& wsize) const;
+
+ private:
+  std::vector<Dictionary*>* dicts_;
+
+  void add_dict(const size_t& wsize);
+  bool acceptable(const char& letter) const;
+  size_t filter_word(std::string& str) const;
+};
+
+#endif //EDITOR_INCLUDES_EDITOR_HPP
