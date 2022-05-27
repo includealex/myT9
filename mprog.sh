caabce96dd2c70b65972b98a7a8ce0bccb78e5e7
includealex <mishin.aa@phystech.edu>
2022-05-27 16:07:35 +0300

    [mprog.sh]

--- mprog.sh
+++ mprog.sh
@@ -5,38 +5,38 @@ cp filestocopy/cache0.txt file.txt
 mkdir forimages
 mkdir build ; cd "$_"
 make -j8
-# make run_editor_main
+make run_editor_main
 
-# for FILE in editor/mprog/plot; do
-#     gnuplot <<- EOF
-#         set xlabel "nthreads"
-#         set ylabel "milliseconds"
-#         set title "Graph title"   
-#         set term png
-#         set output "${FILE}.png"
-#         plot "${FILE}.txt" with lines
-# EOF
+for FILE in editor/mprog/plot; do
+    gnuplot <<- EOF
+        set xlabel "nthreads"
+        set ylabel "milliseconds"
+        set title "Graph title"   
+        set term png
+        set output "${FILE}.png"
+        plot "${FILE}.txt" with lines
+EOF
 
-# mv editor/mprog/plot.png ../forimages/cache0.png
-# done
+mv editor/mprog/plot.png ../forimages/cache0.png
+done
 
-# cd ..
-# cp filestocopy/cache1.txt file.txt
-# cd build
-# make run_editor_main
+cd ..
+cp filestocopy/cache1.txt file.txt
+cd build
+make run_editor_main
 
-# for FILE in editor/mprog/plot; do
-#     gnuplot <<- EOF
-#         set xlabel "nthreads"
-#         set ylabel "milliseconds"
-#         set title "Graph title"   
-#         set term png
-#         set output "${FILE}.png"
-#         plot "${FILE}.txt" with lines
-# EOF
+for FILE in editor/mprog/plot; do
+    gnuplot <<- EOF
+        set xlabel "nthreads"
+        set ylabel "milliseconds"
+        set title "Graph title"   
+        set term png
+        set output "${FILE}.png"
+        plot "${FILE}.txt" with lines
+EOF
 
-# mv editor/mprog/plot.png ../forimages/cache1.png
-# done
+mv editor/mprog/plot.png ../forimages/cache1.png
+done
 
 cd ..
 cp filestocopy/cache2.txt file.txt
